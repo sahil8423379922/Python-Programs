14750e636f4378a7dd71b41274e630ac40e964c9
sahil jaiswal <sahil1905050310026@gmail.com>
2024-02-20 18:07:34 +0530

    Code Updated

---
+++ Program-24-ForLoop.py
@@ -0,0 +1,63 @@
+'''
+1- While
+2- For
+
+range()
+- Range function is use to define range in the for loop
+
+range(initial,final,step)
+range(1,10,1)
+-1
+-2
+-3
+-4
+-5
+-6
+-7
+-8
+-9
+
+range(1,10,2)
+-1
+-3
+-5
+-7
+-9
+
+range(10)
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+range(1,10)
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+
+for x in range(10):
+   print(x)
+
+'''
+a=int(input("Enter a Num"))
+for x in range(1,11):
+  print(x*a)
+  
+  
+  
+  
+
+
