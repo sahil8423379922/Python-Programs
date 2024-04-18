6d654f10a214d2db4a0ac342cc14de2588ece9dc
sahil jaiswal <sahil1905050310026@gmail.com>
2024-04-18 16:08:58 +0530

    code

---
+++ string/PythonString.py
@@ -0,0 +1,72 @@
+# a="computer"
+
+# print(a[0:4:2])
+# print(a[:4])
+# print(a[::1])
+# print(a[::2])
+# print(a[1::2])
+# print(a[4:1:1])
+# print(a[:10:1])
+
+b="computer"
+
+# print(b[-1])
+# print(b[::-1])
+# print(b[-1:-4:-1])
+
+#upper case
+a="computer"
+print(a.upper())
+
+#lower case
+b="COMPUTER"
+print(b.lower())
+
+#strip
+c=" computer is a electronic device "
+print(c)
+print(c.strip())
+
+#replace
+c="computer is a electronic machine"
+print(c.replace("computer","laptop"))
+
+#split function
+c="computer is a  electronic machine"
+print(c.split())
+
+
+a="computer"
+b="is a machine"
+
+c= a+" "+b
+print(c)
+
+
+a="I have {} pens".format(10)
+
+print(a)
+
+n=10
+b=20
+c=n+b
+
+a=f"sum of {n} and {b} is {c}"
+
+print(a)
+
+
+a= "I like to eat \"apple\""
+print(a)
+
+a="computer is a electronic machine computer"
+
+print(a.capitalize())
+
+print(a.count('c'))
+
+print(a.find('i'))
+
+
+
+
